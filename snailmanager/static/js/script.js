81c28a65aee371806fb18299c1eb2a2859872f00
cjns <67065839+cjns@users.noreply.github.com>
2023-12-10 22:52:49 +0000

    Update data and time picker.

--- snailmanager/static/js/script.js
+++ snailmanager/static/js/script.js
@@ -2,4 +2,18 @@
 document.addEventListener('DOMContentLoaded', function () {
     let sidenav = document.querySelectorAll('.sidenav');
     M.Sidenav.init(sidenav);
+
+    // Date Picker
+    var datepicker = document.querySelectorAll('.datepicker');
+    M.Datepicker.init(datepicker, {
+        format: "dd mmmm, yyyy",
+        i18n: {done: "Ok"}
+    });
+
+    // Time Picker
+    var timepicker = document.querySelectorAll('.timepicker');
+    M.Timepicker.init(timepicker, {
+        i18n: {done: "Ok"}
+    });
 });
+
