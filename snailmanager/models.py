61fad56ba43853d06ec90288cc73e6e39b5d15db
cjns <67065839+cjns@users.noreply.github.com>
2023-12-16 19:28:54 +0000

    Add a User class to models.py

--- snailmanager/models.py
+++ snailmanager/models.py
@@ -1,22 +1,29 @@
 from snailmanager import db
+from werkzeug.security import generate_password_hash, check_password_hash
 
 
-# class User(db.Model):
-#     # schema for the User model
-#     user_id = db.Column(db.Integer, primary_key=True)
-#     username = db.Column(db.String(25), unique=True, nullable=False)
-#     email = db.Column(db.String(25), unique=False, nullable=False)
-#     password = db.Column(db.String(), unique=True, nullable=False)
-#     survey = db.relationship("Survey", backref="user", cascade="all, delete", lazy=True)
+class User(db.Model):
+    # schema for the User model
+    user_id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(25), unique=True, nullable=False)
+    email = db.Column(db.String(25), unique=True, nullable=False)
+    password_hash = db.Column(db.String(128), nullable=False)
+    survey = db.relationship('Survey', backref='user', lazy=True)
 
-#     def __repr__(self):
-#         # __repr__ to represent itself in the form of a string
-#         return f"""
-#             User ID: {self.user_id}
-#             Username: {self.username}
-#             Email: {self.email}
-#             Password: {self.password }
-#         """
+    @property
+    def password(self):
+        raise AttributeError('password is not a readable attribute')
+
+    @password.setter
+    def password(self, password):
+        self.password_hash = generate_password_hash(password)
+
+    def verify_password(self, password):
+        return check_password_hash(self.password_hash, password)
+
+    def __repr__(self):
+        # __repr__ to represent itself in the form of a string
+        return f'<User: {self.username}>'
 
 
 class Survey(db.Model):
